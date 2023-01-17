b85a100a8e478dc7805a6de0be5d80092bf96ae8
Jack Wheeler <jack.wheeler@rogers.com>
2023-01-16 23:45:05 -0500

    Testing to compare local build

--- api/index.js
+++ api/index.js
@@ -8,4 +8,9 @@ app.get("/api", (req, res) => {
     res.send("Express on Vercel");
 });
 
+// Initialize server
+app.listen(5000, () => {
+    console.log("Running on port 5000.");
+});
+
 module.exports = app;
